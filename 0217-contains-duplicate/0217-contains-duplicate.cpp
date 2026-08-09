bd77a6a51c46487ea2f75a8a852e091921c8fd72
KUNDAN KUMAR <singhkundan620519@gmail.com>
2026-08-09 23:38:29 +0530

    Time: 75 ms (34.41%), Space: 111.4 MB (20.12%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end()){
+                return true;
+                
+                
+            }
+            mp[nums[i]]=i;
+            
+
+
+        }
+        return false;
+        
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
